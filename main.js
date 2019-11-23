9f7c7f3dacb1c6430e5a92063b7e0a178a120616
Youtirsin <45959799+Youtirsin@users.noreply.github.com>
2019-11-23 21:17:49 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+$(function(){
+    $("body").click(clickAnime);
+});
+function getPos(e){
+    var x = e.originalEvent.x || e.originalEvent.layerX || 0;
+    var y = e.originalEvent.y || e.originalEvent.layerY || 0;
+    return [x,y];
+};
+function clickAnime(e){
+    var Pos=getPos(e);
+    var circle='<div class="cir" style="left:'+(Pos[0])+'px;top:'+(Pos[1])+'px;"></div>';
+    $("#circleContainer").html(circle);
+    $(".cir").animate({
+        "width": "50px",
+        "height": "50px",
+        "opacity":"1",
+        "left":"-=25px",
+        "top":"-=25px"
+    },150,"linear");
+    $(".cir").animate({
+        "width": "100px",
+        "height": "100px",
+        "opacity":"0",
+        "left":"-=25px",
+        "top":"-=25px"
+    },150,"linear",function(){
+        $("#circleContainer").html("");
+    });
+};
